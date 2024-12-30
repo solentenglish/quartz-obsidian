7e81da651fae6366b98d9578acbe53a04aaa97ce
Your Name <you@example.com>
2024-12-30 20:17:29 +0000

    Quartz sync: Dec 30, 2024, 8:17 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "✒️  Glen's Notes",
+    pageTitle: "📓 Glen's Notes",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
